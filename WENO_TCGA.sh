23fd598a9e724509995d7bd185163d45e980ce74
Danny WU <chuniwu@gamble.cs.ucl.ac.uk>
2023-03-10 15:06:05 +0000

    update submission script

--- WENO_TCGA.sh
+++ WENO_TCGA.sh
@@ -1,14 +1,17 @@
 # GPU job script example.
 #$ -S /bin/bash
-#$ -l h_rt=1:0:0
-#$ -l tmem=2G # Initial memory value. Note no h_vmem.
-#$ -wd /home/users/Projects/WENO
+#$ -l h_rt=48:0:0
+#$ -l tmem=16G # Initial memory value
+#$ -l h_vmem=64G
+#$ -wd /home/chuniwu/Projects/WENO
 #$ -j y
-#$ -N Base_WENO_TCGA
+#$ -N WENO_TCGA
 #$ -l gpu=true
-#$ -pe gpu 2 # GPU count multiplies initial memory value.
+#$ -pe gpu 4 # GPU count multiplies initial memory value.
 #$ -R y
 # Commands to be executed follow.
 hostname
 date
-/home/users/Projects/WENO/train_TCGAFeat_BagDistillationDSMIL_SharedEnc_Similarity_StuFilterSmoothed_DropPos.py
+conda activate pytorch
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/chuniwu/.conda/envs/pytorch/lib/
+python3 /home/chuniwu/Projects/WENO/train_TCGAFeat_BagDistillationDSMIL_SharedEnc_Similarity_StuFilterSmoothed_DropPos.py
